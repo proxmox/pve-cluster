0c112ddee427328b1350bdde5a3fb5955c25fe15
Dietmar Maurer <dietmar@proxmox.com>
2013-05-14 10:50:27 +0200

    fix bug 383: resteart pveproxy instead of apache2
    bump version to 3.0-2

--- debian/init.d
+++ debian/init.d
@@ -4,7 +4,7 @@
 # Provides:          pve-cluster
 # Required-Start:    $remote_fs $network $syslog $time
 # Required-Stop:     $remote_fs $network $syslog $time
-# X-Start-Before:    apache2 cron
+# X-Start-Before:    cron
 # Default-Start:     2 3 4 5
 # Default-Stop:      0 1 6
 # Short-Description: Starts the pve cluster filesystem
