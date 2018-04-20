a9d03528d3e88c5ee6b584c597fd9ad62452f82a
Thomas Lamprecht <t.lamprecht@proxmox.com>
2018-04-20 12:01:39 +0200

    fixup: use 'an' for 'HTML5'

--- data/PVE/Cluster.pm
+++ data/PVE/Cluster.pm
@@ -1374,7 +1374,7 @@ my $datacenter_schema = {
 	console => {
 	    optional => 1,
 	    type => 'string',
-	    description => "Select the default Console viewer. You can either use the builtin java applet (VNC; deprecated and maps to html5), an external virt-viewer comtatible application (SPICE), an HTML5 based vnc viewer (noVNC), or a HTML5 based console client (xtermjs). If the selected viewer is not available (e.g. SPICE not activated for the VM), the fallback is noVNC.",
+	    description => "Select the default Console viewer. You can either use the builtin java applet (VNC; deprecated and maps to html5), an external virt-viewer comtatible application (SPICE), an HTML5 based vnc viewer (noVNC), or an HTML5 based console client (xtermjs). If the selected viewer is not available (e.g. SPICE not activated for the VM), the fallback is noVNC.",
 	    enum => ['applet', 'vv', 'html5', 'xtermjs'],
 	},
 	email_from => {
