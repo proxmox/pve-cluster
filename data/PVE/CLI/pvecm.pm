proxmox/pve-cluster

commit 3df092f977c2f592ba4e402f0d99b53a6603d969

fix #1380: pvecm status: add general cluster information
Signed-off-by: Thomas Lamprecht <t.lamprecht@proxmox.com>

data/PVE/CLI/pvecm.pm
@@ -457,6 +457,25 @@ __PACKAGE__->register_method ({
 	my ($param) = @_;
 
 	PVE::Corosync::check_conf_exists();
+	my $conf = eval { PVE::Cluster::cfs_read_file("corosync.conf") } // {};
+	warn "$@" if $@;
+	my $totem = PVE::Corosync::totem_config($conf);
+
+	if (scalar(%$totem)) {
+	    my $print_info = sub {
+		my ($label, $key, $default) = @_;
+		my $val = $totem->{$key} // $default;
+		printf "%-17s %s\n", "$label:", "$val";
+	    };
+
+	    printf "Cluster information\n";
+	    printf "-------------------\n";
+	    $print_info->('Name', 'cluster_name', 'UNKOWN?');
+	    $print_info->('Config Version', 'config_version', -1);
+	    $print_info->('Transport', 'transport', 'knet');
+	    $print_info->('Secure auth', 'secauth', 'off');
+	    printf "\n";
+	}
 
 	my $cmd = ['corosync-quorumtool', '-siH'];
 

commit 75a3d341c54bd622db87a0abf4c6c20a0811a109

corosync: die in check_conf_exists if !$noerr
...and change $silent to $noerr for consistency.

Commit 3df092f9 (fix #1380: pvecm status: add general cluster
information) broke "pvecm status" on non-cluster nodes (well, it made
the error look worse, ofc it didn't "work" before either) because it
tries to access a totem that cannot exist without a corosync.conf.

pvecm status/nodes/expected already fail without a cluster, so it makes
more sense to fail early. But instead of copying the way the qdevice API
handles it, move the die to check_conf_exists directly, which makes
more sense then a warn anyway IMHO.

check_conf_exists is never called without $noerr = 1 outside of
pvecm.pm, so this change does not require any versioned depends/breaks.

Signed-off-by: Stefan Reiter <s.reiter@proxmox.com>

data/PVE/CLI/pvecm.pm
@@ -109,8 +109,7 @@ __PACKAGE__->register_method ({
     code => sub {
 	my ($param) = @_;
 
-	die "Node not in a cluster. Aborting.\n"
-	    if !PVE::Corosync::check_conf_exists(1);
+	PVE::Corosync::check_conf_exists();
 
 	my $members = PVE::Cluster::get_members();
 	foreach my $node (sort keys %$members) {
@@ -266,8 +265,7 @@ __PACKAGE__->register_method ({
     code => sub {
 	my ($param) = @_;
 
-	die "Node not in a cluster. Aborting.\n"
-	    if !PVE::Corosync::check_conf_exists(1);
+	PVE::Corosync::check_conf_exists();
 
 	my $members = PVE::Cluster::get_members();
 	foreach my $node (sort keys %$members) {

data/PVE/Corosync.pm
@@ -184,14 +184,12 @@ PVE::Cluster::cfs_register_file('corosync.conf.new', \&parse_conf,
 				\&write_conf);
 
 sub check_conf_exists {
-    my ($silent) = @_;
-
-    $silent = $silent // 0;
+    my ($noerr) = @_;
 
     my $exists = -f "$basedir/corosync.conf";
 
-    warn "Corosync config '$basedir/corosync.conf' does not exist - is this node part of a cluster?\n"
-	if !$silent && !$exists;
+    die "Error: Corosync config '$basedir/corosync.conf' does not exist - is this node part of a cluster?\n"
+	if !$noerr && !$exists;
 
     return $exists;
 }