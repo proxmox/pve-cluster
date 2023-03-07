581e22f406f7a4becbc33f3b83a6dc4ffed56cdc
Leo Nunner <l.nunner@proxmox.com>
2023-03-07 13:11:03 +0100

    fix #4234: vzdump: add cluster-wide configuration
    Introduce a cluster-wide vzdump.conf file which gets filled with the
    default vzdump configuration.
    
    Signed-off-by: Leo Nunner <l.nunner@proxmox.com>

--- data/src/status.c
+++ data/src/status.c
@@ -89,6 +89,7 @@ static memdb_change_t memdb_change_array[] = {
 	{ .path = "priv/ipam.db" },
 	{ .path = "datacenter.cfg" },
 	{ .path = "vzdump.cron" },
+	{ .path = "vzdump.conf" },
 	{ .path = "jobs.cfg" },
 	{ .path = "ha/crm_commands" },
 	{ .path = "ha/manager_status" },
