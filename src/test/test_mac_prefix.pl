f81da2e34277162edaffe2b90ee61a3f3673aa19
Fabian Grünbichler <f.gruenbichler@proxmox.com>
2026-03-23 15:22:04 +0100

    tests: run make tidy
    Signed-off-by: Fabian Grünbichler <f.gruenbichler@proxmox.com>

--- src/test/test_mac_prefix.pl
+++ src/test/test_mac_prefix.pl
@@ -15,9 +15,16 @@ my $prefix = "00:00:00:00:00:00";
 for (my $i = 0; $i <= length($prefix); $i++) {
     my $sub_prefix = substr($prefix, 0, $i);
     if (2 <= $i && $i <= $longest_prefix_len) {
-        ok(PVE::DataCenterConfig::pve_verify_mac_prefix($sub_prefix), "$sub_prefix is a valid mac prefix");
+        ok(
+            PVE::DataCenterConfig::pve_verify_mac_prefix($sub_prefix),
+            "$sub_prefix is a valid mac prefix",
+        );
     } else {
-        is(PVE::DataCenterConfig::pve_verify_mac_prefix($sub_prefix, 1), undef, "$sub_prefix is not valid mac prefix");
+        is(
+            PVE::DataCenterConfig::pve_verify_mac_prefix($sub_prefix, 1),
+            undef,
+            "$sub_prefix is not valid mac prefix",
+        );
     }
 }
 
